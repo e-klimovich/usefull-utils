c7cb1f578151fed7f89668fd26551f9b30ff595c
Eugene <pochta.korobka@gmail.com>
2019-02-21 20:15:53 +0300

    for..of for objects

---
+++ for-of_with-object.js
@@ -0,0 +1,32 @@
+var myObject = {
+    a: 2,
+    b: 3
+};
+
+Object.defineProperty( myObject, Symbol.iterator, {
+    enumerable: false,
+    writable: false,
+    configurable: true,
+    value: function() {
+        var o = this;
+        var idx = 0;
+        var ks = Object.keys( o );
+        return {
+            next: function() {
+                return {
+                    value: o[ks[idx++]],
+                    done: (idx > ks.length)
+                };
+            }
+        };
+    }
+} ); // this could be replaced with `[Symbol.iterator]: function(){ /* .. */ }` object prop
+
+var it = myObject[Symbol.iterator]();
+it.next(); // { value:2, done:false }
+it.next(); // { value:3, done:false }
+it.next(); // { value:undefined, done:true }
+
+for (var v of myObject) {
+    console.log( v );
+}
